bcdbf233b999366d413cfcffea287f0bf2f68dd6
Despo <tornike.despotashvili@gmail.com>
2024-03-19 16:27:14 +0400

    create database for links

---
+++ src/components/navbar/LinksDB.ts
@@ -0,0 +1,18 @@
+
+import {DashboardIconSVG, DashboardIconInactive} from "../SVGS/DashboardIconSVG"
+ 
+ export const LinkDataBase = [
+    {
+        iconActive: `${''}`,
+        iconInactive: `${''}`,
+        linkName: "Main",
+        path: "/"
+    },
+
+    {
+        iconActive: DashboardIconSVG,
+        iconInactive: DashboardIconInactive,
+        linkName: "Dashboard",
+        path: "/pages/Dashboard"
+    }
+ ]
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,46 +1,21 @@
 import React from "react";
 import NavLink from "./NavLink";
-import { DashboardIconSVG } from "../SVGS/DashboardIconSVG";
+import { LinkDataBase } from "./LinksDB";
 
 const NavBar: React.FC = () => {
-
-  const {dashIconActive, dashIconInactive} = DashboardIconSVG()
-
   return (
     <div className="navbar">
-      <NavLink
-        iconActive={dashIconActive}
-        iconInactive={dashIconInactive}
-        linkName="Main"
-        path="/"
-      />
-      <NavLink
-        iconActive={''}
-        iconInactive={''}
-        linkName="Dashboard"
-        path="/pages/Dashboard"
-        
-      />
-      <NavLink
-        iconActive={''}
-        iconInactive={''}
-        linkName="Link 3"
-        path="/link3"
-        
-      />
-      <NavLink
-        iconActive={''}
-        iconInactive={''}
-        linkName="Link 4"
-        path="/link4"
-        
-      />
-      <NavLink
-        iconActive={''}
-        iconInactive={''}
-        linkName="Link 5"
-        path="/link5"
-      />
+      {LinkDataBase.map((link, index) => {
+        return (
+          <NavLink
+            key={index}
+            iconActive={link.iconActive}
+            iconInactive={link.iconInactive}
+            linkName={link.linkName}
+            path={link.path}
+          />
+        );
+      })}
     </div>
   );
 };
