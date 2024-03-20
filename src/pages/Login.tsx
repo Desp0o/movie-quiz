ac69a81d9fde3b4a5ae9559f246d99ab6634c7ca
Despo <tornike.despotashvili@gmail.com>
2024-03-20 23:12:40 +0400

    update active linksstatus in login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react"
 import Layout from "../components/layout/Layout"
+import { useLinkNameHook } from "../hooks/useLinkNameHook"
 
 const Login = () => {
+
+  const setLinkDispatch = useLinkNameHook()
+
+  useEffect(()=>{
+    setLinkDispatch("")
+  },[])
+
   return (
     <Layout>
       <div>Login</div>
